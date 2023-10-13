e0b98c35bf559e99c187295645d6385b9e692963
FloweringTre <lilianna.carlton@gmail.com>
2023-10-12 22:50:18 -0700

    Added Pumpkin Grand Champion Trophy with its special class. Made it so it appears like a CIT model in an item frame for a 'pretty' display that keeps NBT/Name data.

---
+++ src/main/java/com/kyraltre/tretackshop/block/ModBlocks.java
@@ -0,0 +1,44 @@
+package com.kyraltre.tretackshop.block;
+
+import com.kyraltre.tretackshop.TreTackShop;
+import com.kyraltre.tretackshop.block.custom.PumpkinTrophy;
+import com.kyraltre.tretackshop.item.ModCreativeModTab;
+import com.kyraltre.tretackshop.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, TreTackShop.MOD_ID);
+
+    public static final RegistryObject<Block> PUMPKIN_GRAND = registerBlock("pumpkin_grand",
+            () -> new PumpkinTrophy(BlockBehaviour.Properties.copy(Blocks.PUMPKIN).noOcclusion().strength(0.5f)
+            ), ModCreativeModTab.TRETACK_TAB);
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                           CreativeModeTab tab) {
+            return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                    new Item.Properties().tab(tab)));
+    }
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+}
