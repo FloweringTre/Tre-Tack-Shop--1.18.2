453052673a19aece514287cebf7ce95db964801a
FloweringTre <lilianna.carlton@gmail.com>
2023-10-14 02:36:13 -0700

    renamed file

--- src/main/java/com/kyraltre/tretackshop/item/TackShopCreativeModTab.java
+++ src/main/java/com/kyraltre/tretackshop/item/TackShopCreativeModTab.java
@@ -3,11 +3,11 @@ package com.kyraltre.tretackshop.item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
-public class ModCreativeModTab {
+public class TackShopCreativeModTab {
     public static final CreativeModeTab TRETACK_TAB = new CreativeModeTab("tretacktab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.BUTTERFLY.get()); //this item is the icon of the tab
+            return new ItemStack(TackItems.BUTTERFLY.get()); //this item is the icon of the tab
         }
     };
 }
