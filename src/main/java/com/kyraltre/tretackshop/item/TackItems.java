22ba1d79015b091b1e92da19222369f444964577
FloweringTre <lilianna.carlton@gmail.com>
2023-10-16 05:26:12 -0700

    Added additional Armored Pasture Blanket to the game

--- src/main/java/com/kyraltre/tretackshop/item/TackItems.java
+++ src/main/java/com/kyraltre/tretackshop/item/TackItems.java
@@ -1,5 +1,6 @@
 package com.kyraltre.tretackshop.item;
 
+import com.alaharranhonor.swem.forge.SWEM;
 import com.alaharranhonor.swem.forge.items.SWEMHorseArmorItem;
 import com.alaharranhonor.swem.forge.items.tack.PastureBlanketItem;
 import com.kyraltre.tretackshop.TreTackShop;
@@ -30,7 +31,11 @@ public class TackItems {
 
     public static final RegistryObject<Item> PASTURE_BLANKET_RAINBOW = ITEMS.register("pasture_blanket_rainbow",
             () -> new PastureBlanketItem(SWEMHorseArmorItem.HorseArmorTier.NONE, 0,
-                    "pasture_blanket_rainbow", new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB)));
+                    "pasture_blanket_rainbow", new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(16)));
+    public  static final RegistryObject<Item> PASTURE_BLANKET_RAINBOW_ARMORED = ITEMS.register("pasture_blanket_rainbow_armored",
+            () -> new PastureBlanketItem(SWEMHorseArmorItem.HorseArmorTier.DIAMOND, 37,
+                        "pasture_blanket_rainbow_armored",
+                        (new Item.Properties()).tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(16)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
