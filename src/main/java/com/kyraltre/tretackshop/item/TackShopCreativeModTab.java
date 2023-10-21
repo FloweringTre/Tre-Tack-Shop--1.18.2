243ae88e2d6da31535d3b20ba2cf5d1ecf6f206b
FloweringTre <lilianna.carlton@gmail.com>
2023-10-21 02:35:39 -0700

    Changed Item registering method to just be in the registry instead of split between item and registry

--- src/main/java/com/kyraltre/tretackshop/item/TackShopCreativeModTab.java
+++ src/main/java/com/kyraltre/tretackshop/item/TackShopCreativeModTab.java
@@ -1,5 +1,6 @@
 package com.kyraltre.tretackshop.item;
 
+import com.kyraltre.tretackshop.registry.TackShopItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
@@ -7,7 +8,7 @@ public class TackShopCreativeModTab {
     public static final CreativeModeTab TRETACK_TAB = new CreativeModeTab("tretacktab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(TackItems.BUTTERFLY.get()); //this item is the icon of the tab
+            return new ItemStack(TackShopItems.BUTTERFLY.get()); //this item is the icon of the tab
         }
     };
 }
--- src/main/java/com/kyraltre/tretackshop/registry/TackShopItems.java
+++ src/main/java/com/kyraltre/tretackshop/registry/TackShopItems.java
@@ -23,14 +23,20 @@ import net.minecraftforge.registries.ForgeRegistries.Keys;
 public class TackShopItems {
     public static final DeferredRegister<Item> ITEMS;
 
+    public static final RegistryObject<Item> BUTTERFLY;
+    public static final RegistryObject<HalterItem> HALTER_RAINBOW;
+    public static final List<RegistryObject<HalterItem>> HALTERS;
+    public static final RegistryObject<PastureBlanketItem> PASTURE_BLANKET_RAINBOW;
+    public static final RegistryObject<PastureBlanketItem> PASTURE_BLANKET_RAINBOW_ARMORED;
     public static final List<RegistryObject<PastureBlanketItem>> PASTURE_BLANKETS;
     public static final List<RegistryObject<PastureBlanketItem>> PASTURE_BLANKETS_ARMORED;
     public static final List<RegistryObject<Item>> RIBBON_THREE_TAILS;
     public static final List<RegistryObject<Item>> RIBBON_TWO_TAILS;
     public static final List<RegistryObject<Item>> RIBBON_ONE_TAIL;
     public static final List<RegistryObject<Item>> ROSETTE;
-    public static final List<RegistryObject<HalterItem>> HALTERS;
-
+    public static final RegistryObject<Item> TROPHY_1;
+    public static final RegistryObject<Item> TROPHY_2;
+    public static final RegistryObject<Item> TROPHY_3;
 
 
     public TackShopItems() {
@@ -42,6 +48,24 @@ public class TackShopItems {
 
     static {
         ITEMS = DeferredRegister.create(Keys.ITEMS, "tretackshop");
+
+        BUTTERFLY = ITEMS.register("butterfly",
+                () -> new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB)));
+
+
+        HALTER_RAINBOW = ITEMS.register("halter_rainbow",
+                () -> new HalterItem("halter_rainbow", (new Item.Properties())
+                        .tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(16)));
+
+        PASTURE_BLANKET_RAINBOW = ITEMS.register("pasture_blanket_rainbow",
+                () -> new PastureBlanketItem(SWEMHorseArmorItem.HorseArmorTier.NONE, 0,
+                        "pasture_blanket_rainbow", new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(16)));
+
+        PASTURE_BLANKET_RAINBOW_ARMORED = ITEMS.register("pasture_blanket_rainbow_armored",
+                () -> new PastureBlanketItem(SWEMHorseArmorItem.HorseArmorTier.DIAMOND, 37,
+                        "pasture_blanket_rainbow_armored",
+                        (new Item.Properties()).tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(16)));
+
 
         // 12 Count Items
         PASTURE_BLANKETS = new ArrayList();
@@ -104,6 +128,13 @@ public class TackShopItems {
 
              ++Context.b;
          }
+
+        TROPHY_1 = ITEMS.register("trophy_1",
+                () -> new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB)));
+        TROPHY_2 = ITEMS.register("trophy_2",
+                () -> new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB)));
+        TROPHY_3 = ITEMS.register("trophy_3",
+                () -> new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB)));
 
     }
 }
