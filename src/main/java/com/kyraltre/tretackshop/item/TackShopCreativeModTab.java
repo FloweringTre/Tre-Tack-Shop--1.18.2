360afb1536d18597f21d7f7642f98c8d533cb3c8
FloweringTre <lilianna.carlton@gmail.com>
2023-10-24 13:11:14 -0700

    updated creative mode tab to include a search bar

--- src/main/java/com/kyraltre/tretackshop/item/TackShopCreativeModTab.java
+++ src/main/java/com/kyraltre/tretackshop/item/TackShopCreativeModTab.java
@@ -1,6 +1,8 @@
 package com.kyraltre.tretackshop.item;
 
 import com.kyraltre.tretackshop.registry.TackShopItems;
+import net.minecraft.core.NonNullList;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
@@ -10,8 +12,11 @@ public class TackShopCreativeModTab {
         public ItemStack makeIcon() {
             return new ItemStack(TackShopItems.BUTTERFLY.get()); //this item is the icon of the tab
         }
-    };
-    public boolean hasSearchBar() {
-        return true;
-    }
+        @Override
+        public boolean hasSearchBar() { return true; }
+
+
+
+    }.setBackgroundImage(new ResourceLocation("minecraft","textures/gui/container/creative_inventory/tab_item_search.png"));
+
 }
