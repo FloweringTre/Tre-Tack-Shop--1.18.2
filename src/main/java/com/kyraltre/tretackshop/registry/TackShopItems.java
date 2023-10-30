0726b036379334d81ed6aa8af3b17f9ad332be93
FloweringTre <lilianna.carlton@gmail.com>
2023-10-29 21:37:26 -0700

    Added code for registering all the new ribbons and leg wraps

--- src/main/java/com/kyraltre/tretackshop/registry/TackShopItems.java
+++ src/main/java/com/kyraltre/tretackshop/registry/TackShopItems.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import com.alaharranhonor.swem.forge.items.SWEMHorseArmorItem;
 import com.kyraltre.tretackshop.item.TackShopCreativeModTab;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -29,9 +30,17 @@ public class TackShopItems {
     public static final RegistryObject<AdventureBlanketItem> ADVENTURE_BLANKET_RAINBOW;
     public static final RegistryObject<EnglishBlanketItem> ENGLISH_BLANKET_RAINBOW;
     public static final RegistryObject<WesternBlanketItem> WESTERN_BLANKET_RAINBOW;
+    public static final RegistryObject<AdventureLegWraps> ADVENTURE_LEG_WRAPS_RAINBOW;
+    public static final RegistryObject<LegWrapsItem> ENGLISH_LEG_WRAPS_RAINBOW;
+    public static final RegistryObject<LegWrapsItem> WESTERN_LEG_WRAPS_RAINBOW;
     public static final List<RegistryObject<AdventureBlanketItem>> ADVENTURE_BLANKETS;
     public static final List<RegistryObject<EnglishBlanketItem>> ENGLISH_BLANKETS;
     public static final List<RegistryObject<WesternBlanketItem>> WESTERN_BLANKETS;
+    public static final List<RegistryObject<AdventureLegWraps>> ADVENTURE_LEG_WRAPS;
+    public static final List<RegistryObject<LegWrapsItem>> ENGLISH_LEG_WRAPS;
+    public static final List<RegistryObject<LegWrapsItem>> WESTERN_LEG_WRAPS;
+    public static  final RegistryObject<Item> RIBBON_THREE_TAILS_GRAND;
+    public static  final RegistryObject<Item> RIBBON_THREE_TAILS_RESERVE;
     public static final List<RegistryObject<Item>> RIBBON_THREE_TAILS;
     public static final List<RegistryObject<Item>> RIBBON_TWO_TAILS;
     public static final List<RegistryObject<Item>> RIBBON_ONE_TAIL;
@@ -69,6 +78,18 @@ public class TackShopItems {
                 () -> new EnglishBlanketItem("english_blanket_rainbow", (new Item.Properties())
                         .tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(16)));
 
+        ADVENTURE_LEG_WRAPS_RAINBOW = ITEMS.register("adventure_leg_wraps_rainbow",
+                () -> new AdventureLegWraps("adventure_leg_wraps", (new Item.Properties())
+                    .tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(64)));
+
+        WESTERN_LEG_WRAPS_RAINBOW = ITEMS.register("western_leg_wraps_rainbow",
+                () ->  new WesternLegWraps("western_leg_wraps_rainbow", (new Item.Properties())
+                        .tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(64)));
+
+        ENGLISH_LEG_WRAPS_RAINBOW = ITEMS.register("english_leg_wraps_rainbow",
+                () -> new EnglishLegWraps("english_leg_wraps_rainbow", (new Item.Properties())
+                        .tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(64)));
+
         PASTURE_BLANKET_RAINBOW = ITEMS.register("pasture_blanket_rainbow",
                 () -> new PastureBlanketItem(SWEMHorseArmorItem.HorseArmorTier.NONE, 0,
                         "pasture_blanket_rainbow", new Item.Properties()
@@ -79,6 +100,8 @@ public class TackShopItems {
                         "pasture_blanket_rainbow_armored", (new Item.Properties())
                         .tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(16)));
 
+
+
         // 12 Count Items
         PASTURE_BLANKETS = new ArrayList();
         PASTURE_BLANKETS_ARMORED = new ArrayList();
@@ -87,6 +110,9 @@ public class TackShopItems {
         ADVENTURE_BLANKETS = new ArrayList();
         WESTERN_BLANKETS = new ArrayList();
         ENGLISH_BLANKETS = new ArrayList();
+        ENGLISH_LEG_WRAPS = new ArrayList();
+        WESTERN_LEG_WRAPS = new ArrayList();
+        ADVENTURE_LEG_WRAPS = new ArrayList();
 
         int var1 = 13;
         var rContext = new Object() {
@@ -121,6 +147,21 @@ public class TackShopItems {
                         .tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(16));
             }));
 
+            ADVENTURE_LEG_WRAPS.add(ITEMS.register("adventure_leg_wraps_" + Integer.toString(counter), () -> {
+                return new AdventureLegWraps("adventure_leg_wraps_" + Integer.toString(counter), (new Item.Properties())
+                        .tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(64));
+            }));
+
+            WESTERN_LEG_WRAPS.add(ITEMS.register("western_leg_wraps_" + Integer.toString(counter), () -> {
+                return new WesternLegWraps("western_leg_wraps_" + Integer.toString(counter), (new Item.Properties())
+                        .tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(64));
+            }));
+
+            ENGLISH_LEG_WRAPS.add(ITEMS.register("english_leg_wraps_" + Integer.toString(counter), () -> {
+                return new EnglishLegWraps("english_leg_wraps_" + Integer.toString(counter), (new Item.Properties())
+                        .tab(TackShopCreativeModTab.TRETACK_TAB).stacksTo(64));
+            }));
+
             PASTURE_BLANKETS.add(ITEMS.register("pasture_blanket_" + Integer.toString(counter), () -> {
                 return new PastureBlanketItem(SWEMHorseArmorItem.HorseArmorTier.NONE, 0,
                         "pasture_blanket_" + Integer.toString(counter), (new Item.Properties())
@@ -136,35 +177,35 @@ public class TackShopItems {
             ++rContext.var2;
         }
 
-        // 6 Count Items
+        // Dye Color number items
         RIBBON_THREE_TAILS = new ArrayList();
         RIBBON_TWO_TAILS = new ArrayList();
         RIBBON_ONE_TAIL = new ArrayList();
         ROSETTE = new ArrayList();
-         int r = 7;
-         var Context = new Object() {
-             int b = 1;
-         };
+        DyeColor[] var0 = DyeColor.values();
+        int var3 = var0.length;
 
-         while (Context.b < r) {
-             int counter = Context.b;
-
-             RIBBON_THREE_TAILS.add(ITEMS.register("ribbon_three_tails_" + Integer.toString(counter), () -> {
+        for(int var2 = 0; var2 < var3; ++var2) {
+            DyeColor color = var0[var2];
+             RIBBON_THREE_TAILS.add(ITEMS.register("ribbon_three_tails_" + color.getName(), () -> {
                  return new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB));
              }));
-             RIBBON_TWO_TAILS.add(ITEMS.register("ribbon_two_tails_" + Integer.toString(counter), () -> {
+             RIBBON_TWO_TAILS.add(ITEMS.register("ribbon_two_tails_" + color.getName(), () -> {
                  return new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB));
              }));
-             RIBBON_ONE_TAIL.add(ITEMS.register("ribbon_one_tail_" + Integer.toString(counter), () -> {
+             RIBBON_ONE_TAIL.add(ITEMS.register("ribbon_one_tail_" + color.getName(), () -> {
                  return new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB));
              }));
-             ROSETTE.add(ITEMS.register("rosette_" + Integer.toString(counter), () -> {
+             ROSETTE.add(ITEMS.register("rosette_" + color.getName(), () -> {
                  return new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB));
              }));
 
-             ++Context.b;
          }
 
+        RIBBON_THREE_TAILS_GRAND = ITEMS.register("ribbon_three_tails_grand",
+                () -> new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB)));
+        RIBBON_THREE_TAILS_RESERVE = ITEMS.register("ribbon_three_tails_reserve",
+                () -> new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB)));
         TROPHY_1 = ITEMS.register("trophy_1",
                 () -> new Item(new Item.Properties().tab(TackShopCreativeModTab.TRETACK_TAB)));
         TROPHY_2 = ITEMS.register("trophy_2",
