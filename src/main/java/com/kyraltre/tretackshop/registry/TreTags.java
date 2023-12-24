1e477e2a8405d45d2919506e7e76fde2f78e0055
FloweringTre <lilianna.carlton@gmail.com>
2023-12-24 14:21:52 -0700

    data generation model files for new tack

--- src/main/java/com/kyraltre/tretackshop/registry/TreTags.java
+++ src/main/java/com/kyraltre/tretackshop/registry/TreTags.java
@@ -10,19 +10,35 @@ public class TreTags {
     public static final TagKey<Item> FLYMASKS = ItemTags.create(TreTackShop.resloc("craft/flymasks"));
     public static final TagKey<Item> PASTURE_BLANKETS = ItemTags.create(TreTackShop.resloc("craft/pasture_blankets"));
     public static final TagKey<Item> PASTURE_BLANKETS_ARMORED = ItemTags.create(TreTackShop.resloc("craft/pasture_blankets_armored"));
+    public static final TagKey<Item> QUARTER_SHEETS = ItemTags.create(TreTackShop.resloc("craft/quarter_sheets"));
+
     public static final TagKey<Item> ADVENTURE_BLANKETS = ItemTags.create(TreTackShop.resloc("craft/adventure_blankets"));
     public static final TagKey<Item> ENGLISH_BLANKETS = ItemTags.create(TreTackShop.resloc("craft/english_blankets"));
     public static final TagKey<Item> WESTERN_BLANKETS = ItemTags.create(TreTackShop.resloc("craft/western/blankets"));
+
     public static final TagKey<Item> ADVENTURE_LEG_WRAPS = ItemTags.create(TreTackShop.resloc("craft/adventure_leg_wraps"));
     public static final TagKey<Item> ENGLISH_LEG_WRAPS = ItemTags.create(TreTackShop.resloc("craft/english_leg_wraps"));
     public static final TagKey<Item> WESTERN_LEG_WRAPS = ItemTags.create(TreTackShop.resloc("craft/western/leg_wraps"));
+
     public static final TagKey<Item> ADVENTURE_SADDLES = ItemTags.create(TreTackShop.resloc("craft/adventure_saddles"));
     public static final TagKey<Item> ENGLISH_SADDLES = ItemTags.create(TreTackShop.resloc("craft/english_saddles"));
     public static final TagKey<Item> WESTERN_SADDLES = ItemTags.create(TreTackShop.resloc("craft/western/saddles"));
+
     public static final TagKey<Item> ADVENTURE_BREAST_COLLARS = ItemTags.create(TreTackShop.resloc("craft/adventure_breast_collars"));
     public static final TagKey<Item> ENGLISH_BREAST_COLLARS = ItemTags.create(TreTackShop.resloc("craft/english_breast_collars"));
     public static final TagKey<Item> WESTERN_BREAST_COLLARS = ItemTags.create(TreTackShop.resloc("craft/western/breast_collars"));
     public static final TagKey<Item> CLOTH_BREAST_COLLARS = ItemTags.create(TreTackShop.resloc("craft/cloth_breast_collars"));
+
+    public static final TagKey<Item> ADVENTURE_BRIDLES = ItemTags.create(TreTackShop.resloc("craft/adventure_bridles"));
+    public static final TagKey<Item> ENGLISH_BRIDLES = ItemTags.create(TreTackShop.resloc("craft/english_bridles"));
+    public static final TagKey<Item> WESTERN_BRIDLES = ItemTags.create(TreTackShop.resloc("craft/western/bridles"));
+    public static final TagKey<Item> CLOTH_BRIDLES = ItemTags.create(TreTackShop.resloc("craft/cloth_bridles"));
+
+    public static final TagKey<Item> ADVENTURE_GIRTH_STRAPS = ItemTags.create(TreTackShop.resloc("craft/adventure_girth_straps"));
+    public static final TagKey<Item> ENGLISH_GIRTH_STRAPS = ItemTags.create(TreTackShop.resloc("craft/english_girth_straps"));
+    public static final TagKey<Item> WESTERN_GIRTH_STRAPS = ItemTags.create(TreTackShop.resloc("craft/western/girth_straps"));
+    public static final TagKey<Item> CLOTH_GIRTH_STRAPS = ItemTags.create(TreTackShop.resloc("craft/cloth_girth_straps"));
+
 
     public TreTags () {
 
