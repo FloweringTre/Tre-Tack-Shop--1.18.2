1e79934adc3b557a7356e51ca1915431a122a792
FloweringTre <lilianna.carlton@gmail.com>
2024-03-02 22:52:09 -0700

    work on trying to fix the config files

--- src/main/java/com/kyraltre/tretackshop/config/TackShopCommonConfigs.java
+++ src/main/java/com/kyraltre/tretackshop/config/TackShopCommonConfigs.java
@@ -1,6 +1,9 @@
 package com.kyraltre.tretackshop.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+import org.checkerframework.checker.units.qual.A;
 
 public class TackShopCommonConfigs {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
@@ -12,23 +15,39 @@ public class TackShopCommonConfigs {
     public static final ForgeConfigSpec.ConfigValue<Integer> AWARD_PADDOCK_TACK_SETS;
     public static final ForgeConfigSpec.ConfigValue<Integer> AWARD_ARMOR_EXTRAS_TACK_SETS;
 
+    public static Integer adventureSetsLoaded;
+    public static Integer armorAndSaddleBagSetsLoaded;
+    public static Integer westernSetsLoaded;
+    public static Integer englishSetsLoaded;
+    public static Integer paddockSetsLoaded;
+
     static {
         BUILDER.push("Award Tack Set Configs for Tre's Tack Shop");
         AWARD_ADVENTURE_TACK_SETS = BUILDER.comment(new String[]{"This is for setting how many award tack sets are loaded in to be used. This must be the same for both client and server to properly display the tack items.",
                         "\nThe max value all sets is 48. The default is 14 (the original value loaded).",
                         "Tack sets include the saddle, bridle, saddle pad/blanket, breast collar, leg wraps, and girth straps.",
                         "\n~~~~ADVENTURE TACK~~~~"})
-                .defineInRange("Adventure Sets Loaded", 14, 0, 48);
+                .defineInRange("adventureSetsLoaded", 14, 0, 48);
         AWARD_ARMOR_EXTRAS_TACK_SETS = BUILDER.comment(new String[]{"\n~~~~CLOTH ARMOR & SADDLE BAGS~~~~~", "Separated from the Adventure Tack loading for tailored loaded"})
-                .defineInRange("Armor & Saddle Bag Sets Loaded", 14, 0, 48);
+                .defineInRange("armorAndSaddleBagSetsLoaded", 14, 0, 48);
         AWARD_WESTERN_TACK_SETS = BUILDER.comment(new String[]{"\n~~~~WESTERN TACK~~~~~"})
-                .defineInRange("Western Sets Loaded", 14, 0, 48);
+                .defineInRange("westernSetsLoaded", 14, 0, 48);
         AWARD_ENGLISH_TACK_SETS = BUILDER.comment(new String[]{"\n~~~~ENGLISH TACK~~~~~"})
-                .defineInRange("English Sets Loaded", 14, 0, 48);
+                .defineInRange("englishSetsLoaded", 14, 0, 48);
         AWARD_PADDOCK_TACK_SETS = BUILDER.comment(new String[]{"\n~~~~PADDOCK TACK~~~~~",
                         "These sets include the fly mask, halter, quarter sheets, and both normal and armored pasture blankets"})
-                .defineInRange("Paddock Sets Loaded",14, 0, 48);
+                .defineInRange("paddockSetsLoaded",14, 0, 48);
         BUILDER.pop();
         SPEC = BUILDER.build();
     }
+
+    @SubscribeEvent
+    public static void onLoad(ModConfigEvent event) {
+        adventureSetsLoaded = (Integer)AWARD_ADVENTURE_TACK_SETS.get();
+        armorAndSaddleBagSetsLoaded = (Integer)AWARD_ARMOR_EXTRAS_TACK_SETS.get();
+        westernSetsLoaded = (Integer)AWARD_WESTERN_TACK_SETS.get();
+        englishSetsLoaded = (Integer) AWARD_ENGLISH_TACK_SETS.get();
+        paddockSetsLoaded = (Integer)AWARD_PADDOCK_TACK_SETS.get();
+    }
+
 }
