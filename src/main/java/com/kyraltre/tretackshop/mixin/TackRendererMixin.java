bbfd1972613ab86a31708454e7d08f8dfb93d7b1
FloweringTre <lilianna.carlton@gmail.com>
2024-03-03 23:50:57 -0700

    Mixin file corrected. Double Bridle textures render correctly, as do normal bridles.
    Commented out code for testing the Dyed tack

--- src/main/java/com/kyraltre/tretackshop/mixin/TackRendererMixin.java
+++ src/main/java/com/kyraltre/tretackshop/mixin/TackRendererMixin.java
@@ -5,9 +5,12 @@ import com.alaharranhonor.swem.forge.entities.horse.SWEMHorseEntity;
 import com.alaharranhonor.swem.forge.entities.horse.SWEMHorseEntityBase;
 import com.alaharranhonor.swem.forge.items.tack.AdventureBridleItem;
 import com.alaharranhonor.swem.forge.items.tack.EnglishBridleItem;
+import com.alaharranhonor.swem.forge.items.tack.PastureBlanketItem;
 import com.alaharranhonor.swem.forge.items.tack.WesternBridleItem;
+import com.kyraltre.tretackshop.item.custom.DyedPastureBlanket;
 import com.kyraltre.tretackshop.item.custom.PelhamBridleItem;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.world.item.DyeableArmorItem;
 import net.minecraft.world.item.ItemStack;
 import org.checkerframework.checker.units.qual.A;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,12 +30,13 @@ public abstract class TackRendererMixin extends ExtendedGeoEntityRenderer<SWEMHo
 
     private static final String[] PELHAM_BRIDLE_BONE_NAMES = new String[]{"WesternBridle", "WesternBridleLeftBit", "WesternBridleRightBit", "EnglishBridle", "EnglishBridleLeftBit", "EnglishBridleRightBit"};
     private static final String[] PELHAM_BRIDLE_REIN_BONE_NAMES = new String[]{"EnglishBridleLeftRein", "EnglishBridleLeftRein1", "EnglishBridleLeftRein2", "EnglishBridleRightRein", "EnglishBridleRightRein1", "EnglishBridleRightRein2", "WesternBridleLeftRein", "WesternBridleLeftRein1", "WesternBridleLeftRein2", "WesternBridleRightRein", "WesternBridleRightRein1", "WesternBridleRightRein2"};
+    private static final String[] PASTURE_BLANKET_BONE_NAMES = new String[]{"PBFrontLeft", "PBMiddle", "PBFrontRight", "PBBack"};
 
     protected TackRendererMixin(EntityRendererProvider.Context renderManager, AnimatedGeoModel<SWEMHorseEntity> modelProvider) {
         super(renderManager, modelProvider);
     }
 
-
+// This Injection resets the ENTIRE check bridle method to combine all bridles to render collectively. All bridles will render both bits and rein options for any combination a user wants.
     @Inject(
             at = @At(value = "HEAD"),
             method = "checkBridlesForRendering",
@@ -67,6 +71,44 @@ public abstract class TackRendererMixin extends ExtendedGeoEntityRenderer<SWEMHo
             });
         }
    }
+
+//    @Inject(
+//            at = @At(value = "HEAD"),
+//            method = "checkPastureBlanketForRendering",
+//            cancellable = true,
+//            remap = false
+//    )
+//    private void checkPastureBlanketForRendering(SWEMHorseEntity entity, CallbackInfo info) {
+//        info.cancel();
+//
+//        ItemStack stack = entity.getSWEMArmor();
+//        ItemStack itemstack = pLivingEntity.getItemBySlot(pSlot);
+//        if (stack.getItem() instanceof PastureBlanketItem) {
+//            Arrays.stream(PASTURE_BLANKET_BONE_NAMES).forEach((boneName) -> {
+//                this.showBone(boneName, entity);
+//            });
+//            this.hideBone("BreastLeft", entity);
+//            this.hideBone("BreastRight", entity);
+//        } else {
+//            Arrays.stream(PASTURE_BLANKET_BONE_NAMES).forEach((boneName) -> {
+//                this.hideBone(boneName, entity);
+//            });
+//            this.showBone("BreastLeft", entity);
+//            this.showBone("BreastRight", entity);
+//        }
+//        if (stack.getItem() instanceof DyedPastureBlanket) {
+//            int i = ((net.minecraft.world.item.DyeableLeatherItem) DyedPastureBlanket).getColor(itemstack);
+//            float f = (float) (i >> 16 & 255) / 255.0F;
+//            float f1 = (float) (i >> 8 & 255) / 255.0F;
+//            float f2 = (float) (i & 255) / 255.0F;
+//            this.renderModel(pPoseStack, pBuffer, p_117123_, flag1, model, f, f1, f2, this.getArmorResource(pLivingEntity, itemstack, pSlot, null));
+//            this.renderModel(pPoseStack, pBuffer, p_117123_, flag1, model, 1.0F, 1.0F, 1.0F, this.getArmorResource(pLivingEntity, itemstack, pSlot, "overlay"));
+//        } else {
+//            this.renderModel(pPoseStack, pBuffer, p_117123_, flag1, model, 1.0F, 1.0F, 1.0F, this.getArmorResource(pLivingEntity, itemstack, pSlot, null));
+//        }
+//    }
+
+
 
     public void hideBone(String boneName, SWEMHorseEntity entity) {
         this.getGeoModelProvider().getModel(this.getGeoModelProvider().getModelLocation(entity)).getBone(boneName).ifPresent((bone) -> {
