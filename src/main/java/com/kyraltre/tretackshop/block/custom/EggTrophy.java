49d454d4c0a71c98a50cbf4899493abf1f08db68
FloweringTre <lilianna.carlton@gmail.com>
2024-03-10 15:10:41 -0700

    Reworked the egg trophies json files since I couldnt make them dyeable

--- src/main/java/com/kyraltre/tretackshop/block/custom/EggTrophy.java
+++ src/main/java/com/kyraltre/tretackshop/block/custom/EggTrophy.java
@@ -1,70 +1,38 @@
 package com.kyraltre.tretackshop.block.custom;
 
-import com.alaharranhonor.swem.forge.registry.SWEMBlockEntities;
-import com.alaharranhonor.swem.forge.registry.SWEMItems;
-import com.alaharranhonor.swem.forge.tileentity.CowboyHatBE;
-import com.alaharranhonor.swem.forge.util.DualDyeableItem;
-import com.kyraltre.tretackshop.registry.AwardShopItems;
-import com.kyraltre.tretackshop.registry.TackShopBlockEntities;
-import com.kyraltre.tretackshop.registry.TackShopItems;
-import com.kyraltre.tretackshop.tileentity.EggTrophyBE;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
-import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-import java.util.function.Supplier;
+import java.util.HashMap;
+import java.util.Map;
 
-public class EggTrophy extends Block implements DualDyeableItem {
+public class EggTrophy extends Block {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
 
-    public static final VoxelShape SHAPE = Block.box(6, 0, 6, 10, 12, 10);
+    public EggTrophy(Properties pProperties) {
+        super(pProperties);
+    }
+
+    public static final VoxelShape SHAPE = Block.box(6, 0, 6, 10, 11, 10);
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE;
     }
 
-    public final String texturePath;
 
-    public EggTrophy(String texturePath, Properties pProperties) {
-        super(pProperties);
-        this.texturePath = texturePath;
-        }
-//to drop the block entity with data instead of the no data block
-    public List<ItemStack> getDrops(BlockState pState, LootContext.Builder builder) {
-        List<ItemStack> drops = super.getDrops(pState, builder);
-        Object var5 = builder.getParameter(LootContextParams.BLOCK_ENTITY);
-        if (var5 instanceof EggTrophyBE egg) {
-            drops.add(egg.getEgg());
-        }
-
-        return drops;
-    }
-
-    //connected entity
-    public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        return new EggTrophyBE((String)this.texturePath, pos, state);
-    }
 
     /* FACING CODE FROM FURNACE*/
     @Override
@@ -86,6 +54,5 @@ public class EggTrophy extends Block implements DualDyeableItem {
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(FACING);
     }
-
 
 }
